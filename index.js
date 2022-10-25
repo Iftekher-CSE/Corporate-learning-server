6072d9dcf54ab38975c7851dcd66e741c09e7288
Iftekher-CSE <iftekher.cse@gmail.com>
2022-10-26 00:08:23 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
+
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const course = courses.find(cr => cr.id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {
